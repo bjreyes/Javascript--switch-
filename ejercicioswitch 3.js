abd7950c3fe363b4b740d3613d7fed739355279e
bjreyes <brunorebu12@gmail.com>
2014-06-30 18:47:39 -0500

    ejercicios
    comiteado de ejercicios en javascript con sublime text 2

---
+++ ejercicioswitch 3.js
@@ -0,0 +1,28 @@
+//Ejercicio Java (switch) 3
+ //variables
+ var codigo, precio, desc, icompra,ipagar, cantidad
+
+ //entrada de datos
+ codigo= cil
+ cantidad = 3
+
+ // proceso
+ switch (codigo)
+ {
+ 	case "primor":
+ 	 precio = 5.99;
+ 	 break;
+ 	case "girasol":
+ 	 precio = 5.50;
+ 	 break;
+  	case "cil":
+     precio = 4.50;
+     break;
+    case "cociner":
+     precio = 4.70;
+     break;
+ }
+
+if(cantidad > 10) {
+  
+}
