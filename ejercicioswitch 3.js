3a66646a5b23d6bc6d2c678b42a479c92c1c8614
Jose Luis Manrique Cabana <kenshin2273@gmail.com>
2014-07-01 11:31:38 -0500

    corrección al indentado y logica

--- ejercicioswitch 3.js
+++ ejercicioswitch 3.js
@@ -1,43 +1,43 @@
 //Ejercicio Java (switch) 3
  //variables
- var codigo, precio, desc, icompra,ipagar, cantidad
+ var codigo, precio, desc, icompra,ipagar, cantidad;
 
  //entrada de datos
- codigo= cil
- cantidad = 3
+ codigo= cil;
+ cantidad = 3;
 
  // proceso
  switch (codigo)
  {
- 	case "primor":
+     case "primor":
  	 precio = 5.99;
- 	 break;
- 	case "girasol":
+     	 break;
+     case "girasol":
  	 precio = 5.50;
  	 break;
-  	case "cil":
-     precio = 4.50;
-     break;
-    case "cociner":
-     precio = 4.70;
-     break;
+     case "cil":
+         precio = 4.50;
+         break;
+     case "cociner":
+         precio = 4.70;
+         break;
  }
 
-icompra = precio * cantidad
+icompra = precio * cantidad;
 
 if(cantidad >= 10) {
-	desc = icompra * 0.125;
-}else {
-if (cantidad >= 7) {
-	desc = icompra * 0.10;
-}else {
-if (cantidad >= 4) {
-	desc = icompra * 0.075;
-}else {
-	desc= icompra * 0.05;
-}
+   desc = icompra * 0.125;
+} else {
+   if (cantidad >= 7) {
+      desc = icompra * 0.10;
+   } else {
+      if (cantidad >= 4) {
+	  desc = icompra * 0.075;
+      } else {
+	  desc= icompra * 0.05;
+      }
+   }	
 }	
-}	
-}
 
-ipagar = icompra - desc 	
+ipagar = icompra - desc;
+console.log(ipagar);
